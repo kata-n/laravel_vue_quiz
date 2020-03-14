a6efcae8c05ba1c741ec873ac0ea0d43de8181fa
kata-n <kata.kawa813@gmail.com>
2020-03-14 13:05:26 +0900

    dev add9

---
+++ app/Information.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon
+
+class Information extends Model
+{
+  protected $table = 'informations'
+
+  public function getCreatedAtAttribute($table){
+    return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y/m/d');
+  }
+}
