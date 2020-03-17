d2a8a391cddb46b7214605691a03ce6b91de3a99
kata-n <kata.kawa813@gmail.com>
2020-03-17 21:30:59 +0900

    dev12

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/css/app.css">
-  <title>クイズ画面</title>
-</head>
-<body>
-  <div id="app">
-    <router-view />
-  </div>
-  <script src="/js/app.js"></script>
-</body>
-</html>
+@extends('layouts.app')
