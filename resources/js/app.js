348ea0e588e06e041687aabb350241db1244f84d
kata-n <kata.kawa813@gmail.com>
2020-03-21 09:23:22 +0900

    add14

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@ import SocialSharing from 'vue-social-sharing'
 import axios from 'axios'
 import jQuery from "jquery";
 
-// ここから追加
 import MainPage from './components/page/MainPage'
-// ここまで追加
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 window.$ = window.jQuery = jQuery;
 require('bootstrap');
